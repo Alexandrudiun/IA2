2e88b8ff2a7afe0d82fec54464cd85a1da6f5be7
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2024-12-04 09:28:14 +0200

    Create Scurt.m

---
+++ Lab3/Scurt.m
@@ -0,0 +1,32 @@
+function [mv, sigma2, Me] = ab311(n)
+    d=day(datetime('now'));
+    
+    v = d + (2*d)*randn(1,n); 
+
+    mv = mean(v);
+    sigma2 = var(v);
+    Me = median(v);
+        
+    figure;
+    histogram(vector, 'FaceColor', 'green');
+    grid on;
+    title('Histogramă a setului de date generat');
+    xlabel('Valori');
+    ylabel('Frecvență');
+    legend('Set de date');
+
+
+confidence_interval = [mv - 3*sqrt(sigma2)/sqrt(n), mv + 3*sqrt(sigma2)/sqrt(n)];
+
+disp('Rezultate statistice:');
+disp('Media aritmetică:');
+disp(mv);
+disp('Estimația erorii standard (varianța):');
+disp(sigma2);
+disp('Mediana de selecție:');
+disp(Me);
+disp('Interval de încredere (η = 0.9973):');
+disp(confidence_interval);
+
+end
+
